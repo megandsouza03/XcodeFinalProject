e80ea0663b47e11d9d8e35847567d9e887e92588
Karan Magdani <magdani.k@husky.neu.edu>
2018-04-13 01:45:23 -0400

    added logic for side view navigation + some other rubbish

---
+++ FinalProject/FinalProject/HomePageViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomePageViewController.swift
+//  FinalProject
+//
+//  Created by karan magdani on 4/12/18.
+//  Copyright © 2018 Swift Final Project. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sideMenus()
+        customizeNavBar()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sideMenus() {
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 160
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func customizeNavBar(){
+        
+        navigationController?.navigationBar.tintColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(red: 255/255, green: 87/255, blue: 35/255, alpha: 1)
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: UIColor.white], for: .selected)
+        
+    }
+
+}
